972cc26ce7e81a431f4b6db3c4760b487655b960
zxh0 <zhangxiuhong2013@gmail.com>
2018-01-08 15:21:24 +0800

    ch09 -> ch10

---
+++ code/go/ch10/src/luago/vm/inst_misc.go
@@ -0,0 +1,22 @@
+package vm
+
+import . "luago/api"
+
+// R(A) := R(B)
+func move(i Instruction, vm LuaVM) {
+	a, b, _ := i.ABC()
+	a += 1
+	b += 1
+
+	vm.Copy(b, a)
+}
+
+// pc+=sBx; if (A) close all upvalues >= R(A - 1)
+func jmp(i Instruction, vm LuaVM) {
+	a, sBx := i.AsBx()
+
+	vm.AddPC(sBx)
+	if a != 0 {
+		panic("todo: jmp!")
+	}
+}
