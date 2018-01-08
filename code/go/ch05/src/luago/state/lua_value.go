7df6226f80dc104d9f78a523ffc2bc78844709dd
zxh0 <zhangxiuhong2013@gmail.com>
2018-01-08 15:13:45 +0800

    * ch05

--- code/go/ch05/src/luago/state/lua_value.go
+++ code/go/ch05/src/luago/state/lua_value.go
@@ -1,6 +1,7 @@
 package state
 
 import . "luago/api"
+import "luago/number"
 
 type luaValue interface{}
 
@@ -28,4 +29,42 @@ func convertToBoolean(val luaValue) bool {
 	default:
 		return true
 	}
+}
+
+// http://www.lua.org/manual/5.3/manual.html#3.4.3
+func convertToFloat(val luaValue) (float64, bool) {
+	switch x := val.(type) {
+	case int64:
+		return float64(x), true
+	case float64:
+		return x, true
+	case string:
+		return number.ParseFloat(x)
+	default:
+		return 0, false
+	}
+}
+
+// http://www.lua.org/manual/5.3/manual.html#3.4.3
+func convertToInteger(val luaValue) (int64, bool) {
+	switch x := val.(type) {
+	case int64:
+		return x, true
+	case float64:
+		return number.FloatToInteger(x)
+	case string:
+		return _stringToInteger(x)
+	default:
+		return 0, false
+	}
+}
+
+func _stringToInteger(s string) (int64, bool) {
+	if i, ok := number.ParseInteger(s); ok {
+		return i, true
+	}
+	if f, ok := number.ParseFloat(s); ok {
+		return number.FloatToInteger(f)
+	}
+	return 0, false
 }
