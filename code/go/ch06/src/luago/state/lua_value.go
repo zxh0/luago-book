c587f9da39b2fa2dbf0b148054e8839e00576982
zxh0 <zhangxiuhong2013@gmail.com>
2018-01-08 15:15:18 +0800

    ch05 -> ch06

---
+++ code/go/ch06/src/luago/state/lua_value.go
@@ -0,0 +1,70 @@
+package state
+
+import . "luago/api"
+import "luago/number"
+
+type luaValue interface{}
+
+func typeOf(val luaValue) LuaType {
+	switch val.(type) {
+	case nil:
+		return LUA_TNIL
+	case bool:
+		return LUA_TBOOLEAN
+	case int64, float64:
+		return LUA_TNUMBER
+	case string:
+		return LUA_TSTRING
+	default:
+		panic("todo!")
+	}
+}
+
+func convertToBoolean(val luaValue) bool {
+	switch x := val.(type) {
+	case nil:
+		return false
+	case bool:
+		return x
+	default:
+		return true
+	}
+}
+
+// http://www.lua.org/manual/5.3/manual.html#3.4.3
+func convertToFloat(val luaValue) (float64, bool) {
+	switch x := val.(type) {
+	case int64:
+		return float64(x), true
+	case float64:
+		return x, true
+	case string:
+		return number.ParseFloat(x)
+	default:
+		return 0, false
+	}
+}
+
+// http://www.lua.org/manual/5.3/manual.html#3.4.3
+func convertToInteger(val luaValue) (int64, bool) {
+	switch x := val.(type) {
+	case int64:
+		return x, true
+	case float64:
+		return number.FloatToInteger(x)
+	case string:
+		return _stringToInteger(x)
+	default:
+		return 0, false
+	}
+}
+
+func _stringToInteger(s string) (int64, bool) {
+	if i, ok := number.ParseInteger(s); ok {
+		return i, true
+	}
+	if f, ok := number.ParseFloat(s); ok {
+		return number.FloatToInteger(f)
+	}
+	return 0, false
+}
