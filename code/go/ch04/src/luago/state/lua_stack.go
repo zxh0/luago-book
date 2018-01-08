3be9232813483bd6d59a33d327f9c0621e906a95
zxh0 <zhangxiuhong2013@gmail.com>
2018-01-08 15:06:21 +0800

    * ch04

---
+++ code/go/ch04/src/luago/state/lua_stack.go
@@ -0,0 +1,76 @@
+package state
+
+type luaStack struct {
+	slots []luaValue
+	top   int
+}
+
+func newLuaStack(size int) *luaStack {
+	return &luaStack{
+		slots: make([]luaValue, size),
+		top:   0,
+	}
+}
+
+func (self *luaStack) check(n int) {
+	free := len(self.slots) - self.top
+	for i := free; i < n; i++ {
+		self.slots = append(self.slots, nil)
+	}
+}
+
+func (self *luaStack) push(val luaValue) {
+	if self.top == len(self.slots) {
+		panic("stack overflow!")
+	}
+	self.slots[self.top] = val
+	self.top++
+}
+
+func (self *luaStack) pop() luaValue {
+	if self.top < 1 {
+		panic("stack underflow!")
+	}
+	self.top--
+	val := self.slots[self.top]
+	self.slots[self.top] = nil
+	return val
+}
+
+func (self *luaStack) absIndex(idx int) int {
+	if idx >= 0 {
+		return idx
+	}
+	return idx + self.top + 1
+}
+
+func (self *luaStack) isValid(idx int) bool {
+	absIdx := self.absIndex(idx)
+	return absIdx > 0 && absIdx <= self.top
+}
+
+func (self *luaStack) get(idx int) luaValue {
+	absIdx := self.absIndex(idx)
+	if absIdx > 0 && absIdx <= self.top {
+		return self.slots[absIdx-1]
+	}
+	return nil
+}
+
+func (self *luaStack) set(idx int, val luaValue) {
+	absIdx := self.absIndex(idx)
+	if absIdx > 0 && absIdx <= self.top {
+		self.slots[absIdx-1] = val
+		return
+	}
+	panic("invalid index!")
+}
+
+func (self *luaStack) reverse(from, to int) {
+	slots := self.slots
+	for from < to {
+		slots[from], slots[to] = slots[to], slots[from]
+		from++
+		to--
+	}
+}
