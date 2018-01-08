48ec2381858259db62684b940a0e4b906b49a091
zxh0 <zhangxiuhong2013@gmail.com>
2018-01-08 15:12:35 +0800

    ch04 -> ch05

---
+++ code/go/ch05/src/luago/state/lua_value.go
@@ -0,0 +1,31 @@
+package state
+
+import . "luago/api"
+
+type luaValue interface{}
+
+func typeOf(val luaValue) LuaType {
+	switch val.(type) {
+	case nil:
+		return LUA_TNIL
+	case bool:
+		return LUA_TBOOLEAN
+	case int64, float64:
+		return LUA_TNUMBER
+	case string:
+		return LUA_TSTRING
+	default:
+		panic("todo!")
+	}
+}
+
+func convertToBoolean(val luaValue) bool {
+	switch x := val.(type) {
+	case nil:
+		return false
+	case bool:
+		return x
+	default:
+		return true
+	}
+}
