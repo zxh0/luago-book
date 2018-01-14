9f55c1031a0a992b1c61f596de03e497d3563657
zxh0 <zhangxiuhong2013@gmail.com>
2018-01-14 18:21:05 +0800

    ch11 -> ch12

---
+++ code/go/ch12/src/luago/binchunk/binary_chunk.go
@@ -0,0 +1,79 @@
+package binchunk
+
+const (
+	LUA_SIGNATURE    = "\x1bLua"
+	LUAC_VERSION     = 0x53
+	LUAC_FORMAT      = 0
+	LUAC_DATA        = "\x19\x93\r\n\x1a\n"
+	CINT_SIZE        = 4
+	CSZIET_SIZE      = 8
+	INSTRUCTION_SIZE = 4
+	LUA_INTEGER_SIZE = 8
+	LUA_NUMBER_SIZE  = 8
+	LUAC_INT         = 0x5678
+	LUAC_NUM         = 370.5
+)
+
+const (
+	TAG_NIL       = 0x00
+	TAG_BOOLEAN   = 0x01
+	TAG_NUMBER    = 0x03
+	TAG_INTEGER   = 0x13
+	TAG_SHORT_STR = 0x04
+	TAG_LONG_STR  = 0x14
+)
+
+type binaryChunk struct {
+	header
+	sizeUpvalues byte // ?
+	mainFunc     *Prototype
+}
+
+type header struct {
+	signature       [4]byte
+	version         byte
+	format          byte
+	luacData        [6]byte
+	cintSize        byte
+	sizetSize       byte
+	instructionSize byte
+	luaIntegerSize  byte
+	luaNumberSize   byte
+	luacInt         int64
+	luacNum         float64
+}
+
+// function prototype
+type Prototype struct {
+	Source          string // debug
+	LineDefined     uint32
+	LastLineDefined uint32
+	NumParams       byte
+	IsVararg        byte
+	MaxStackSize    byte
+	Code            []uint32
+	Constants       []interface{}
+	Upvalues        []Upvalue
+	Protos          []*Prototype
+	LineInfo        []uint32 // debug
+	LocVars         []LocVar // debug
+	UpvalueNames    []string // debug
+}
+
+type Upvalue struct {
+	Instack byte
+	Idx     byte
+}
+
+type LocVar struct {
+	VarName string
+	StartPC uint32
+	EndPC   uint32
+}
+
+func Undump(data []byte) *Prototype {
+	reader := &reader{data}
+	reader.checkHeader()
+	reader.readByte() // size_upvalues
+	return reader.readProto("")
+}
---
+++ code/go/ch12/src/luago/state/lua_value.go
@@ -0,0 +1,120 @@
+package state
+
+import "fmt"
+import . "luago/api"
+import "luago/number"
+
+type luaValue interface{}
+
+func typeOf(val luaValue) LuaType {
+	switch val.(type) {
+	case nil:
+		return LUA_TNIL
+	case bool:
+		return LUA_TBOOLEAN
+	case int64, float64:
+		return LUA_TNUMBER
+	case string:
+		return LUA_TSTRING
+	case *luaTable:
+		return LUA_TTABLE
+	case *closure:
+		return LUA_TFUNCTION
+	default:
+		panic("todo!")
+	}
+}
+
+func convertToBoolean(val luaValue) bool {
+	switch x := val.(type) {
+	case nil:
+		return false
+	case bool:
+		return x
+	default:
+		return true
+	}
+}
+
+// http://www.lua.org/manual/5.3/manual.html#3.4.3
+func convertToFloat(val luaValue) (float64, bool) {
+	switch x := val.(type) {
+	case int64:
+		return float64(x), true
+	case float64:
+		return x, true
+	case string:
+		return number.ParseFloat(x)
+	default:
+		return 0, false
+	}
+}
+
+// http://www.lua.org/manual/5.3/manual.html#3.4.3
+func convertToInteger(val luaValue) (int64, bool) {
+	switch x := val.(type) {
+	case int64:
+		return x, true
+	case float64:
+		return number.FloatToInteger(x)
+	case string:
+		return _stringToInteger(x)
+	default:
+		return 0, false
+	}
+}
+
+func _stringToInteger(s string) (int64, bool) {
+	if i, ok := number.ParseInteger(s); ok {
+		return i, true
+	}
+	if f, ok := number.ParseFloat(s); ok {
+		return number.FloatToInteger(f)
+	}
+	return 0, false
+}
+
+/* metatable */
+
+func getMetatable(val luaValue, ls *luaState) *luaTable {
+	if t, ok := val.(*luaTable); ok {
+		return t.metatable
+	}
+	key := fmt.Sprintf("_MT%d", typeOf(val))
+	if mt := ls.registry.get(key); mt != nil {
+		return mt.(*luaTable)
+	}
+	return nil
+}
+
+func setMetatable(val luaValue, mt *luaTable, ls *luaState) {
+	if t, ok := val.(*luaTable); ok {
+		t.metatable = mt
+		return
+	}
+	key := fmt.Sprintf("_MT%d", typeOf(val))
+	ls.registry.put(key, mt)
+}
+
+func getMetafield(val luaValue, fieldName string, ls *luaState) luaValue {
+	if mt := getMetatable(val, ls); mt != nil {
+		return mt.get(fieldName)
+	}
+	return nil
+}
+
+func callMetamethod(a, b luaValue, mmName string, ls *luaState) (luaValue, bool) {
+	var mm luaValue
+	if mm = getMetafield(a, mmName, ls); mm == nil {
+		if mm = getMetafield(b, mmName, ls); mm == nil {
+			return nil, false
+		}
+	}
+
+	ls.stack.check(4)
+	ls.stack.push(mm)
+	ls.stack.push(a)
+	ls.stack.push(b)
+	ls.Call(2, 1)
+	return ls.stack.pop(), true
+}
