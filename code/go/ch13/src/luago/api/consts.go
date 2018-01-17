4853a36c346d403efe3e543f23645a3a4ad013c8
zxh0 <zhangxiuhong2013@gmail.com>
2018-01-17 14:25:06 +0800

    ch12 -> ch13

---
+++ code/go/ch13/src/luago/api/consts.go
@@ -0,0 +1,45 @@
+package api
+
+const LUA_MINSTACK = 20
+const LUAI_MAXSTACK = 1000000
+const LUA_REGISTRYINDEX = -LUAI_MAXSTACK - 1000
+const LUA_RIDX_GLOBALS int64 = 2
+
+/* basic types */
+const (
+	LUA_TNONE = iota - 1 // -1
+	LUA_TNIL
+	LUA_TBOOLEAN
+	LUA_TLIGHTUSERDATA
+	LUA_TNUMBER
+	LUA_TSTRING
+	LUA_TTABLE
+	LUA_TFUNCTION
+	LUA_TUSERDATA
+	LUA_TTHREAD
+)
+
+/* arithmetic functions */
+const (
+	LUA_OPADD  = iota // +
+	LUA_OPSUB         // -
+	LUA_OPMUL         // *
+	LUA_OPMOD         // %
+	LUA_OPPOW         // ^
+	LUA_OPDIV         // /
+	LUA_OPIDIV        // //
+	LUA_OPBAND        // &
+	LUA_OPBOR         // |
+	LUA_OPBXOR        // ~
+	LUA_OPSHL         // <<
+	LUA_OPSHR         // >>
+	LUA_OPUNM         // -
+	LUA_OPBNOT        // ~
+)
+
+/* comparison functions */
+const (
+	LUA_OPEQ = iota // ==
+	LUA_OPLT        // <
+	LUA_OPLE        // <=
+)
---
+++ code/go/ch13/src/luago/main.go
@@ -0,0 +1,91 @@
+package main
+
+import "fmt"
+import "io/ioutil"
+import "os"
+import . "luago/api"
+import "luago/state"
+
+func main() {
+	if len(os.Args) > 1 {
+		data, err := ioutil.ReadFile(os.Args[1])
+		if err != nil {
+			panic(err)
+		}
+
+		ls := state.New()
+		ls.Register("print", print)
+		ls.Register("getmetatable", getMetatable)
+		ls.Register("setmetatable", setMetatable)
+		ls.Register("next", next)
+		ls.Register("pairs", pairs)
+		ls.Register("ipairs", iPairs)
+		ls.Load(data, "chunk", "b")
+		ls.Call(0, 0)
+	}
+}
+
+func print(ls LuaState) int {
+	nArgs := ls.GetTop()
+	for i := 1; i <= nArgs; i++ {
+		if ls.IsBoolean(i) {
+			fmt.Printf("%t", ls.ToBoolean(i))
+		} else if ls.IsString(i) {
+			s, _ := ls.ToString(i)
+			fmt.Print(s)
+		} else {
+			fmt.Print(ls.TypeName(ls.Type(i)))
+		}
+		if i < nArgs {
+			fmt.Print("\t")
+		}
+	}
+	fmt.Println()
+	return 0
+}
+
+func getMetatable(ls LuaState) int {
+	if !ls.GetMetatable(1) {
+		ls.PushNil()
+	}
+	return 1
+}
+
+func setMetatable(ls LuaState) int {
+	ls.SetMetatable(1)
+	return 1
+}
+
+func next(ls LuaState) int {
+	ls.SetTop(2) /* create a 2nd argument if there isn't one */
+	if ls.Next(1) {
+		return 2
+	} else {
+		ls.PushNil()
+		return 1
+	}
+}
+
+func pairs(ls LuaState) int {
+	ls.PushGoFunction(next) /* will return generator, */
+	ls.PushValue(1)         /* state, */
+	ls.PushNil()
+	return 3
+}
+
+func iPairs(ls LuaState) int {
+	ls.PushGoFunction(_iPairsAux) /* iteration function */
+	ls.PushValue(1)               /* state */
+	ls.PushInteger(0)             /* initial value */
+	return 3
+}
+
+func _iPairsAux(ls LuaState) int {
+	i := ls.ToInteger(2) + 1
+	ls.PushInteger(i)
+	if ls.GetI(1, i) == LUA_TNIL {
+		return 1
+	} else {
+		return 2
+	}
+}
