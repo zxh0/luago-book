6d6f421c6011f369167de10f949baf07eff889bc
zxh0 <zhangxiuhong2013@gmail.com>
2018-01-20 14:05:48 +0800

    ch13 -> ch14

---
+++ code/go/ch14/src/luago/state/api_call.go
@@ -0,0 +1,150 @@
+package state
+
+import . "luago/api"
+import "luago/binchunk"
+import "luago/vm"
+
+// [-0, +1, –]
+// http://www.lua.org/manual/5.3/manual.html#lua_load
+func (self *luaState) Load(chunk []byte, chunkName, mode string) int {
+	proto := binchunk.Undump(chunk) // todo
+	c := newLuaClosure(proto)
+	self.stack.push(c)
+	if len(proto.Upvalues) > 0 {
+		env := self.registry.get(LUA_RIDX_GLOBALS)
+		c.upvals[0] = &env
+	}
+	return LUA_OK
+}
+
+// [-(nargs+1), +nresults, e]
+// http://www.lua.org/manual/5.3/manual.html#lua_call
+func (self *luaState) Call(nArgs, nResults int) {
+	val := self.stack.get(-(nArgs + 1))
+
+	c, ok := val.(*closure)
+	if !ok {
+		if mf := getMetafield(val, "__call", self); mf != nil {
+			if c, ok = mf.(*closure); ok {
+				self.stack.push(val)
+				self.Insert(-(nArgs + 2))
+				nArgs += 1
+			}
+		}
+	}
+
+	if ok {
+		if c.proto != nil {
+			self.callLuaClosure(nArgs, nResults, c)
+		} else {
+			self.callGoClosure(nArgs, nResults, c)
+		}
+	} else {
+		panic("not function!")
+	}
+}
+
+func (self *luaState) callGoClosure(nArgs, nResults int, c *closure) {
+	// create new lua stack
+	newStack := newLuaStack(nArgs+LUA_MINSTACK, self)
+	newStack.closure = c
+
+	// pass args, pop func
+	if nArgs > 0 {
+		args := self.stack.popN(nArgs)
+		newStack.pushN(args, nArgs)
+	}
+	self.stack.pop()
+
+	// run closure
+	self.pushLuaStack(newStack)
+	r := c.goFunc(self)
+	self.popLuaStack()
+
+	// return results
+	if nResults != 0 {
+		results := newStack.popN(r)
+		self.stack.pushN(results, nResults)
+	}
+}
+
+func (self *luaState) callLuaClosure(nArgs, nResults int, c *closure) {
+	nRegs := int(c.proto.MaxStackSize)
+	nParams := int(c.proto.NumParams)
+	isVararg := c.proto.IsVararg == 1
+
+	// create new lua stack
+	newStack := newLuaStack(nRegs+LUA_MINSTACK, self)
+	newStack.closure = c
+
+	// pass args, pop func
+	funcAndArgs := self.stack.popN(nArgs + 1)
+	newStack.pushN(funcAndArgs[1:], nParams)
+	newStack.top = nRegs
+	if nArgs > nParams && isVararg {
+		newStack.varargs = funcAndArgs[nParams+1:]
+	}
+
+	// run closure
+	self.pushLuaStack(newStack)
+	self.runLuaClosure()
+	self.popLuaStack()
+
+	// return results
+	if nResults != 0 {
+		results := newStack.popN(newStack.top - nRegs)
+		self.stack.check(len(results))
+		self.stack.pushN(results, nResults)
+	}
+}
+
+func (self *luaState) runLuaClosure() {
+	for {
+		inst := vm.Instruction(self.Fetch())
+		inst.Execute(self)
+		if inst.Opcode() == vm.OP_RETURN {
+			break
+		}
+	}
+}
+
+// Calls a function in protected mode.
+// http://www.lua.org/manual/5.3/manual.html#lua_pcall
+func (self *luaState) PCall(nArgs, nResults, msgh int) (status int) {
+	caller := self.stack
+
+	// catch error
+	defer func() {
+		if r := recover(); r != nil { // todo
+			if msgh != 0 {
+				panic(r)
+			} else {
+				for self.stack != caller {
+					self.popLuaStack()
+				}
+				self.stack.push(_getErrObj(r))
+				status = LUA_ERRRUN
+			}
+		}
+	}()
+
+	self.Call(nArgs, nResults)
+	status = LUA_OK
+	return
+}
+
+func _getErrObj(err interface{}) luaValue {
+	if t, ok := err.(*luaTable); ok {
+		return t.get("_ERR")
+	}
+
+	// runtime error
+	switch x := err.(type) {
+	case string:
+		return x
+	case error:
+		return x.Error()
+	default:
+		return "unknown error"
+	}
+}
---
+++ code/go/ch14/src/luago/state/api_set.go
@@ -0,0 +1,104 @@
+package state
+
+import . "luago/api"
+
+// [-2, +0, e]
+// http://www.lua.org/manual/5.3/manual.html#lua_settable
+func (self *luaState) SetTable(idx int) {
+	t := self.stack.get(idx)
+	v := self.stack.pop()
+	k := self.stack.pop()
+	self.setTable(t, k, v, false)
+}
+
+// [-1, +0, e]
+// http://www.lua.org/manual/5.3/manual.html#lua_setfield
+func (self *luaState) SetField(idx int, k string) {
+	t := self.stack.get(idx)
+	v := self.stack.pop()
+	self.setTable(t, k, v, false)
+}
+
+// [-1, +0, e]
+// http://www.lua.org/manual/5.3/manual.html#lua_seti
+func (self *luaState) SetI(idx int, i int64) {
+	t := self.stack.get(idx)
+	v := self.stack.pop()
+	self.setTable(t, i, v, false)
+}
+
+// [-2, +0, m]
+// http://www.lua.org/manual/5.3/manual.html#lua_rawset
+func (self *luaState) RawSet(idx int) {
+	t := self.stack.get(idx)
+	v := self.stack.pop()
+	k := self.stack.pop()
+	self.setTable(t, k, v, true)
+}
+
+// [-1, +0, m]
+// http://www.lua.org/manual/5.3/manual.html#lua_rawseti
+func (self *luaState) RawSetI(idx int, i int64) {
+	t := self.stack.get(idx)
+	v := self.stack.pop()
+	self.setTable(t, i, v, true)
+}
+
+// [-1, +0, e]
+// http://www.lua.org/manual/5.3/manual.html#lua_setglobal
+func (self *luaState) SetGlobal(name string) {
+	t := self.registry.get(LUA_RIDX_GLOBALS)
+	v := self.stack.pop()
+	self.setTable(t, name, v, false)
+}
+
+// [-0, +0, e]
+// http://www.lua.org/manual/5.3/manual.html#lua_register
+func (self *luaState) Register(name string, f GoFunction) {
+	self.PushGoFunction(f)
+	self.SetGlobal(name)
+}
+
+// [-1, +0, –]
+// http://www.lua.org/manual/5.3/manual.html#lua_setmetatable
+func (self *luaState) SetMetatable(idx int) {
+	val := self.stack.get(idx)
+	mtVal := self.stack.pop()
+
+	if mtVal == nil {
+		setMetatable(val, nil, self)
+	} else if mt, ok := mtVal.(*luaTable); ok {
+		setMetatable(val, mt, self)
+	} else {
+		panic("table expected!") // todo
+	}
+}
+
+// t[k]=v
+func (self *luaState) setTable(t, k, v luaValue, raw bool) {
+	if tbl, ok := t.(*luaTable); ok {
+		if raw || tbl.get(k) != nil || !tbl.hasMetafield("__newindex") {
+			tbl.put(k, v)
+			return
+		}
+	}
+
+	if !raw {
+		if mf := getMetafield(t, "__newindex", self); mf != nil {
+			switch x := mf.(type) {
+			case *luaTable:
+				self.setTable(x, k, v, true)
+				return
+			case *closure:
+				self.stack.push(mf)
+				self.stack.push(t)
+				self.stack.push(k)
+				self.stack.push(v)
+				self.Call(3, 0)
+				return
+			}
+		}
+	}
+
+	panic("index error!")
+}
