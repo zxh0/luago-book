e206eb2071588f41796d91a2f97345790870efbd
zxh0 <zhangxiuhong2013@gmail.com>
2018-01-20 14:05:48 +0800

    ch13 -> ch14

---
+++ code/go/ch14/src/luago/api/lua_state.go
@@ -0,0 +1,89 @@
+package api
+
+type LuaType = int
+type ArithOp = int
+type CompareOp = int
+
+type GoFunction func(LuaState) int
+
+func LuaUpvalueIndex(i int) int {
+	return LUA_REGISTRYINDEX - i
+}
+
+type LuaState interface {
+	/* basic stack manipulation */
+	GetTop() int
+	AbsIndex(idx int) int
+	CheckStack(n int) bool
+	Pop(n int)
+	Copy(fromIdx, toIdx int)
+	PushValue(idx int)
+	Replace(idx int)
+	Insert(idx int)
+	Remove(idx int)
+	Rotate(idx, n int)
+	SetTop(idx int)
+	/* access functions (stack -> Go) */
+	TypeName(tp LuaType) string
+	Type(idx int) LuaType
+	IsNone(idx int) bool
+	IsNil(idx int) bool
+	IsNoneOrNil(idx int) bool
+	IsBoolean(idx int) bool
+	IsInteger(idx int) bool
+	IsNumber(idx int) bool
+	IsString(idx int) bool
+	IsTable(idx int) bool
+	IsThread(idx int) bool
+	IsFunction(idx int) bool
+	IsGoFunction(idx int) bool
+	ToBoolean(idx int) bool
+	ToInteger(idx int) int64
+	ToIntegerX(idx int) (int64, bool)
+	ToNumber(idx int) float64
+	ToNumberX(idx int) (float64, bool)
+	ToString(idx int) (string, bool)
+	ToGoFunction(idx int) GoFunction
+	RawLen(idx int) uint
+	/* push functions (Go -> stack) */
+	PushNil()
+	PushBoolean(b bool)
+	PushInteger(n int64)
+	PushNumber(n float64)
+	PushString(s string)
+	PushGoFunction(f GoFunction)
+	PushGoClosure(f GoFunction, n int)
+	PushGlobalTable()
+	/* Comparison and arithmetic functions */
+	Arith(op ArithOp)
+	Compare(idx1, idx2 int, op CompareOp) bool
+	RawEqual(idx1, idx2 int) bool
+	/* get functions (Lua -> stack) */
+	NewTable()
+	CreateTable(nArr, nRec int)
+	GetTable(idx int) LuaType
+	GetField(idx int, k string) LuaType
+	GetI(idx int, i int64) LuaType
+	RawGet(idx int) LuaType
+	RawGetI(idx int, i int64) LuaType
+	GetMetatable(idx int) bool
+	GetGlobal(name string) LuaType
+	/* set functions (stack -> Lua) */
+	SetTable(idx int)
+	SetField(idx int, k string)
+	SetI(idx int, i int64)
+	RawSet(idx int)
+	RawSetI(idx int, i int64)
+	SetMetatable(idx int)
+	SetGlobal(name string)
+	Register(name string, f GoFunction)
+	/* 'load' and 'call' functions (load and run Lua code) */
+	Load(chunk []byte, chunkName, mode string) int
+	Call(nArgs, nResults int)
+	PCall(nArgs, nResults, msgh int) int
+	/* miscellaneous functions */
+	Len(idx int)
+	Concat(n int)
+	Next(idx int) bool
+	Error() int
+}
---
+++ code/go/ch14/src/luago/vm/inst_table.go
@@ -0,0 +1,73 @@
+package vm
+
+import . "luago/api"
+
+/* number of list items to accumulate before a SETLIST instruction */
+const LFIELDS_PER_FLUSH = 50
+
+// R(A) := {} (size = B,C)
+func newTable(i Instruction, vm LuaVM) {
+	a, b, c := i.ABC()
+	a += 1
+
+	vm.CreateTable(Fb2int(b), Fb2int(c))
+	vm.Replace(a)
+}
+
+// R(A) := R(B)[RK(C)]
+func getTable(i Instruction, vm LuaVM) {
+	a, b, c := i.ABC()
+	a += 1
+	b += 1
+
+	vm.GetRK(c)
+	vm.GetTable(b)
+	vm.Replace(a)
+}
+
+// R(A)[RK(B)] := RK(C)
+func setTable(i Instruction, vm LuaVM) {
+	a, b, c := i.ABC()
+	a += 1
+
+	vm.GetRK(b)
+	vm.GetRK(c)
+	vm.SetTable(a)
+}
+
+// R(A)[(C-1)*FPF+i] := R(A+i), 1 <= i <= B
+func setList(i Instruction, vm LuaVM) {
+	a, b, c := i.ABC()
+	a += 1
+
+	if c > 0 {
+		c = c - 1
+	} else {
+		c = Instruction(vm.Fetch()).Ax()
+	}
+
+	bIsZero := b == 0
+	if bIsZero {
+		b = int(vm.ToInteger(-1)) - a - 1
+		vm.Pop(1)
+	}
+
+	vm.CheckStack(1)
+	idx := int64(c * LFIELDS_PER_FLUSH)
+	for j := 1; j <= b; j++ {
+		idx++
+		vm.PushValue(a + j)
+		vm.SetI(a, idx)
+	}
+
+	if bIsZero {
+		for j := vm.RegisterCount() + 1; j <= vm.GetTop(); j++ {
+			idx++
+			vm.PushValue(j)
+			vm.SetI(a, idx)
+		}
+
+		// clear stack
+		vm.SetTop(vm.RegisterCount())
+	}
+}
