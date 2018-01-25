3c8ebb8657a68a076b6b8844d564624fcbc15d73
zxh0 <zhangxiuhong2013@gmail.com>
2018-01-25 14:49:12 +0800

    ch15 -> ch16

---
+++ code/go/ch16/src/luago/state/api_get.go
@@ -0,0 +1,102 @@
+package state
+
+import . "luago/api"
+
+// [-0, +1, m]
+// http://www.lua.org/manual/5.3/manual.html#lua_newtable
+func (self *luaState) NewTable() {
+	self.CreateTable(0, 0)
+}
+
+// [-0, +1, m]
+// http://www.lua.org/manual/5.3/manual.html#lua_createtable
+func (self *luaState) CreateTable(nArr, nRec int) {
+	t := newLuaTable(nArr, nRec)
+	self.stack.push(t)
+}
+
+// [-1, +1, e]
+// http://www.lua.org/manual/5.3/manual.html#lua_gettable
+func (self *luaState) GetTable(idx int) LuaType {
+	t := self.stack.get(idx)
+	k := self.stack.pop()
+	return self.getTable(t, k, false)
+}
+
+// [-0, +1, e]
+// http://www.lua.org/manual/5.3/manual.html#lua_getfield
+func (self *luaState) GetField(idx int, k string) LuaType {
+	t := self.stack.get(idx)
+	return self.getTable(t, k, false)
+}
+
+// [-0, +1, e]
+// http://www.lua.org/manual/5.3/manual.html#lua_geti
+func (self *luaState) GetI(idx int, i int64) LuaType {
+	t := self.stack.get(idx)
+	return self.getTable(t, i, false)
+}
+
+// [-1, +1, –]
+// http://www.lua.org/manual/5.3/manual.html#lua_rawget
+func (self *luaState) RawGet(idx int) LuaType {
+	t := self.stack.get(idx)
+	k := self.stack.pop()
+	return self.getTable(t, k, true)
+}
+
+// [-0, +1, –]
+// http://www.lua.org/manual/5.3/manual.html#lua_rawgeti
+func (self *luaState) RawGetI(idx int, i int64) LuaType {
+	t := self.stack.get(idx)
+	return self.getTable(t, i, true)
+}
+
+// [-0, +1, e]
+// http://www.lua.org/manual/5.3/manual.html#lua_getglobal
+func (self *luaState) GetGlobal(name string) LuaType {
+	t := self.registry.get(LUA_RIDX_GLOBALS)
+	return self.getTable(t, name, false)
+}
+
+// [-0, +(0|1), –]
+// http://www.lua.org/manual/5.3/manual.html#lua_getmetatable
+func (self *luaState) GetMetatable(idx int) bool {
+	val := self.stack.get(idx)
+
+	if mt := getMetatable(val, self); mt != nil {
+		self.stack.push(mt)
+		return true
+	} else {
+		return false
+	}
+}
+
+// push(t[k])
+func (self *luaState) getTable(t, k luaValue, raw bool) LuaType {
+	if tbl, ok := t.(*luaTable); ok {
+		v := tbl.get(k)
+		if raw || v != nil || !tbl.hasMetafield("__index") {
+			self.stack.push(v)
+			return typeOf(v)
+		}
+	}
+
+	if !raw {
+		if mf := getMetafield(t, "__index", self); mf != nil {
+			switch x := mf.(type) {
+			case *luaTable:
+				return self.getTable(x, k, true)
+			case *closure:
+				self.stack.push(mf)
+				self.stack.push(t)
+				self.stack.push(k)
+				self.Call(2, 1)
+				v := self.stack.get(-1)
+				return typeOf(v)
+			}
+		}
+	}
+
+	panic("index error!")
+}
