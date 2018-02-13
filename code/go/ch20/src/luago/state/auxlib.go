362aca4471f7fa290e7a58674fee5815a1625044
zxh0 <zhangxiuhong2013@gmail.com>
2018-02-13 22:26:54 +0800

    * ch20

--- code/go/ch20/src/luago/state/auxlib.go
+++ code/go/ch20/src/luago/state/auxlib.go
@@ -258,12 +258,13 @@ func (self *luaState) CallMeta(obj int, event string) bool {
 // http://www.lua.org/manual/5.3/manual.html#luaL_openlibs
 func (self *luaState) OpenLibs() {
 	libs := map[string]GoFunction{
-		"_G":     stdlib.OpenBaseLib,
-		"math":   stdlib.OpenMathLib,
-		"table":  stdlib.OpenTableLib,
-		"string": stdlib.OpenStringLib,
-		"utf8":   stdlib.OpenUTF8Lib,
-		"os":     stdlib.OpenOSLib,
+		"_G":      stdlib.OpenBaseLib,
+		"math":    stdlib.OpenMathLib,
+		"table":   stdlib.OpenTableLib,
+		"string":  stdlib.OpenStringLib,
+		"utf8":    stdlib.OpenUTF8Lib,
+		"os":      stdlib.OpenOSLib,
+		"package": stdlib.OpenPackageLib,
 	}
 
 	for name, fun := range libs {
