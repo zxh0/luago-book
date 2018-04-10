07e761b67428edec7b002a281b420b866813672d
zxh0 <zhangxiuhong2013@gmail.com>
2018-04-10 11:32:11 +0800

    ch14 -> ch15

--- code/go/ch15/src/luago/state/closure.go
+++ code/go/ch15/src/luago/state/closure.go
@@ -3,14 +3,18 @@ package state
 import . "luago/api"
 import "luago/binchunk"
 
+type upvalue struct {
+	val *luaValue
+}
+
 type closure struct {
 	proto  *binchunk.Prototype // lua closure
 	goFunc GoFunction          // go closure
-	upvals []*luaValue
+	upvals []*upvalue
 }
 
 func newLuaClosure(proto *binchunk.Prototype) *closure {
-	upvals := make([]*luaValue, len(proto.Upvalues))
+	upvals := make([]*upvalue, len(proto.Upvalues))
 	return &closure{
 		proto:  proto,
 		upvals: upvals,
@@ -18,7 +22,7 @@ func newLuaClosure(proto *binchunk.Prototype) *closure {
 }
 
 func newGoClosure(f GoFunction, nUpvals int) *closure {
-	upvals := make([]*luaValue, nUpvals)
+	upvals := make([]*upvalue, nUpvals)
 	return &closure{
 		goFunc: f,
 		upvals: upvals,
