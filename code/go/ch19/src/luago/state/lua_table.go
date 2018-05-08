f47b35e189bf8a3810d9f23d828b29203e4e7d4d
zxh0 <zhangxiuhong2013@gmail.com>
2018-05-08 09:39:27 +0800

    ch13+ : fix nextKey()

--- code/go/ch19/src/luago/state/lua_table.go
+++ code/go/ch19/src/luago/state/lua_table.go
@@ -5,9 +5,11 @@ import "luago/number"
 
 type luaTable struct {
 	metatable *luaTable
-	_map      map[luaValue]luaValue
 	arr       []luaValue
-	keys      []luaValue // used by next()
+	_map      map[luaValue]luaValue
+	keys      map[luaValue]luaValue // used by next()
+	lastKey   luaValue              // used by next()
+	changed   bool                  // used by next()
 }
 
 func newLuaTable(nArr, nRec int) *luaTable {
@@ -57,6 +59,7 @@ func (self *luaTable) put(key, val luaValue) {
 		panic("table index is NaN!")
 	}
 
+	self.changed = true
 	key = _floatToIntger(key)
 	if idx, ok := key.(int64); ok && idx >= 1 {
 		arrLen := int64(len(self.arr))
@@ -106,25 +109,33 @@ func (self *luaTable) _expandArray() {
 }
 
 func (self *luaTable) nextKey(key luaValue) luaValue {
-	if key == nil {
+	if self.keys == nil || (key == nil && self.changed) {
 		self.initKeys()
+		self.changed = false
 	}
-	if len(self.keys) > 0 {
-		kextKey := self.keys[0]
-		self.keys = self.keys[1:]
-		return kextKey
+
+	nextKey := self.keys[key]
+	if nextKey == nil && key != nil && key != self.lastKey {
+		panic("invalid key to 'next'")
 	}
-	return nil
+
+	return nextKey
 }
 
 func (self *luaTable) initKeys() {
-	self.keys = nil
+	self.keys = make(map[luaValue]luaValue)
+	var key luaValue = nil
 	for i, v := range self.arr {
 		if v != nil {
-			self.keys = append(self.keys, int64(i+1))
+			self.keys[key] = int64(i + 1)
+			key = int64(i + 1)
 		}
 	}
-	for k, _ := range self._map {
-		self.keys = append(self.keys, k)
+	for k, v := range self._map {
+		if v != nil {
+			self.keys[key] = k
+			key = k
+		}
 	}
+	self.lastKey = key
 }
