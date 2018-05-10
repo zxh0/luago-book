de75c0bde17061ff691523034c79277c25724b1f
zxh0 <zhangxiuhong2013@gmail.com>
2018-05-10 18:09:58 +0800

    ch19+ : fix table library

--- code/go/ch19/src/luago/stdlib/lib_table.go
+++ code/go/ch19/src/luago/stdlib/lib_table.go
@@ -4,6 +4,8 @@ import "sort"
 import "strings"
 import . "luago/api"
 
+const MAX_LEN = 1000000 // TODO
+
 /*
 ** Operations that an object must define to mimic a table
 ** (some functions only need some of them)
@@ -192,11 +194,12 @@ func tabUnpack(ls LuaState) int {
 	if i > e { /* empty range */
 		return 0
 	}
-	// n = (lua_Unsigned)e - i;  /* number of elements minus 1 (avoid overflows) */
-	// if (n >= (unsigned int)INT_MAX  || !lua_checkstack(L, (int)(++n)))
-	//   return luaL_error(L, "too many results to unpack");
+
 	n := int(e - i + 1)
-	ls.CheckStack(n)
+	if n <= 0 || n >= MAX_LEN || !ls.CheckStack(n) {
+		return ls.Error2("too many results to unpack")
+	}
+
 	for ; i < e; i++ { /* push arg[i..e - 1] (to avoid overflows) */
 		ls.GetI(1, i)
 	}
@@ -209,7 +212,9 @@ func tabUnpack(ls LuaState) int {
 // table.sort (list [, comp])
 // http://www.lua.org/manual/5.3/manual.html#pdf-table.sort
 func tabSort(ls LuaState) int {
-	sort.Sort(wrapper{ls})
+	w := wrapper{ls}
+	ls.ArgCheck(w.Len() < MAX_LEN, 1, "array too big")
+	sort.Sort(w)
 	return 0
 }
 
@@ -223,7 +228,7 @@ func (self wrapper) Len() int {
 
 func (self wrapper) Less(i, j int) bool {
 	ls := self.ls
-	if ls.GetTop() == 2 { // cmp is given
+	if ls.IsFunction(2) { // cmp is given
 		ls.PushValue(2)
 		ls.GetI(1, int64(i+1))
 		ls.GetI(1, int64(j+1))
