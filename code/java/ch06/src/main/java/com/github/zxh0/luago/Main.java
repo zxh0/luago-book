62030690b89992868cc0f58404bb5ee3c5c6b6ec
zxh0 <zhangxiuhong2013@gmail.com>
2018-08-03 18:59:30 +0800

    java: ch06

--- code/java/ch06/src/main/java/com/github/zxh0/luago/Main.java
+++ code/java/ch06/src/main/java/com/github/zxh0/luago/Main.java
@@ -2,32 +2,42 @@ package com.github.zxh0.luago;
 
 import com.github.zxh0.luago.api.LuaState;
 import com.github.zxh0.luago.api.LuaType;
+import com.github.zxh0.luago.api.LuaVM;
+import com.github.zxh0.luago.binchunk.BinaryChunk;
+import com.github.zxh0.luago.binchunk.Prototype;
 import com.github.zxh0.luago.state.LuaStateImpl;
+import com.github.zxh0.luago.vm.Instruction;
+import com.github.zxh0.luago.vm.OpCode;
 
-import static com.github.zxh0.luago.api.ArithOp.*;
-import static com.github.zxh0.luago.api.CmpOp.*;
-import static com.github.zxh0.luago.api.LuaType.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Main {
 
-    public static void main(String[] args) {
-        LuaState ls = new LuaStateImpl();
-        ls.pushInteger(1);
-        ls.pushString("2.0");
-        ls.pushString("3.0");
-        ls.pushNumber(4.0);
-        printStack(ls);
+    public static void main(String[] args) throws Exception {
+        if (args.length > 0) {
+            byte[] data = Files.readAllBytes(Paths.get(args[0]));
+            Prototype proto = BinaryChunk.undump(data);
+            luaMain(proto);
+        }
+    }
 
-        ls.arith(LUA_OPADD);
-        printStack(ls);
-        ls.arith(LUA_OPBNOT);
-        printStack(ls);
-        ls.len(2);
-        printStack(ls);
-        ls.concat(3);
-        printStack(ls);
-        ls.pushBoolean(ls.compare(1, 2, LUA_OPEQ));
-        printStack(ls);
+    private static void luaMain(Prototype proto) {
+        LuaVM vm = new LuaStateImpl(proto);
+        vm.setTop(proto.getMaxStackSize());
+        for (;;) {
+            int pc = vm.getPC();
+            int i = vm.fetch();
+            OpCode opCode = Instruction.getOpCode(i);
+            if (opCode != OpCode.RETURN) {
+                opCode.getAction().execute(i, vm);
+
+                System.out.printf("[%02d] %-8s ", pc+1, opCode.name());
+                printStack(vm);
+            } else {
+                break;
+            }
+        }
     }
 
     private static void printStack(LuaState ls) {
