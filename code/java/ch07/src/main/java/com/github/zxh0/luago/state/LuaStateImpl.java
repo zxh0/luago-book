9cc7b940b55ad7e032534ad2b266ace0284d061c
zxh0 <zhangxiuhong2013@gmail.com>
2018-08-03 18:59:48 +0800

    java: ch06->ch07

---
+++ code/java/ch07/src/main/java/com/github/zxh0/luago/state/LuaStateImpl.java
@@ -0,0 +1,346 @@
+package com.github.zxh0.luago.state;
+
+import com.github.zxh0.luago.api.*;
+import com.github.zxh0.luago.binchunk.Prototype;
+
+import static com.github.zxh0.luago.api.ArithOp.*;
+import static com.github.zxh0.luago.api.LuaType.*;
+
+public class LuaStateImpl implements LuaState, LuaVM {
+
+    private LuaStack stack = new LuaStack();
+    private Prototype proto;
+    private int pc;
+
+    public LuaStateImpl(Prototype proto) {
+        this.proto = proto;
+    }
+
+    public LuaStateImpl() {
+        proto = null;
+    }
+
+    /* basic stack manipulation */
+
+    @Override
+    public int getTop() {
+        return stack.top();
+    }
+
+    @Override
+    public int absIndex(int idx) {
+        return stack.absIndex(idx);
+    }
+
+    @Override
+    public boolean checkStack(int n) {
+        return true; // TODO
+    }
+
+    @Override
+    public void pop(int n) {
+        for (int i = 0; i < n; i++) {
+            stack.pop();
+        }
+    }
+
+    @Override
+    public void copy(int fromIdx, int toIdx) {
+        stack.set(toIdx, stack.get(fromIdx));
+    }
+
+    @Override
+    public void pushValue(int idx) {
+        stack.push(stack.get(idx));
+    }
+
+    @Override
+    public void replace(int idx) {
+        stack.set(idx, stack.pop());
+    }
+
+    @Override
+    public void insert(int idx) {
+        rotate(idx, 1);
+    }
+
+    @Override
+    public void remove(int idx) {
+        rotate(idx, -1);
+        pop(1);
+    }
+
+    @Override
+    public void rotate(int idx, int n) {
+        int t = stack.top() - 1;            /* end of stack segment being rotated */
+        int p = stack.absIndex(idx) - 1;    /* start of segment */
+        int m = n >= 0 ? t - n : p - n - 1; /* end of prefix */
+
+        stack.reverse(p, m);     /* reverse the prefix with length 'n' */
+        stack.reverse(m + 1, t); /* reverse the suffix */
+        stack.reverse(p, t);     /* reverse the entire segment */
+    }
+
+    @Override
+    public void setTop(int idx) {
+        int newTop = stack.absIndex(idx);
+        if (newTop < 0) {
+            throw new RuntimeException("stack underflow!");
+        }
+
+        int n = stack.top() - newTop;
+        if (n > 0) {
+            for (int i = 0; i < n; i++) {
+                stack.pop();
+            }
+        } else if (n < 0) {
+            for (int i = 0; i > n; i--) {
+                stack.push(null);
+            }
+        }
+    }
+
+    /* access functions (stack -> Go); */
+
+    @Override
+    public String typeName(LuaType tp) {
+        switch (tp) {
+            case LUA_TNONE:     return "no value";
+            case LUA_TNIL:      return "nil";
+            case LUA_TBOOLEAN:  return "boolean";
+            case LUA_TNUMBER:   return "number";
+            case LUA_TSTRING:   return "string";
+            case LUA_TTABLE:    return "table";
+            case LUA_TFUNCTION: return "function";
+            case LUA_TTHREAD:   return "thread";
+            default:            return "userdata";
+        }
+    }
+
+    @Override
+    public LuaType type(int idx) {
+        return stack.isValid(idx)
+                ? LuaValue.typeOf(stack.get(idx))
+                : LUA_TNONE;
+    }
+
+    @Override
+    public boolean isNone(int idx) {
+        return type(idx) == LUA_TNONE;
+    }
+
+    @Override
+    public boolean isNil(int idx) {
+        return type(idx) == LUA_TNIL;
+    }
+
+    @Override
+    public boolean isNoneOrNil(int idx) {
+        LuaType t = type(idx);
+        return t == LUA_TNONE || t == LUA_TNIL;
+    }
+
+    @Override
+    public boolean isBoolean(int idx) {
+        return type(idx) == LUA_TBOOLEAN;
+    }
+
+    @Override
+    public boolean isInteger(int idx) {
+        return stack.get(idx) instanceof Long;
+    }
+
+    @Override
+    public boolean isNumber(int idx) {
+        return toNumberX(idx) != null;
+    }
+
+    @Override
+    public boolean isString(int idx) {
+        LuaType t = type(idx);
+        return t == LUA_TSTRING || t == LUA_TNUMBER;
+    }
+
+    @Override
+    public boolean isTable(int idx) {
+        return type(idx) == LUA_TTABLE;
+    }
+
+    @Override
+    public boolean isThread(int idx) {
+        return type(idx) == LUA_TTHREAD;
+    }
+
+    @Override
+    public boolean isFunction(int idx) {
+        return type(idx) == LUA_TFUNCTION;
+    }
+
+    @Override
+    public boolean toBoolean(int idx) {
+        return LuaValue.toBoolean(stack.get(idx));
+    }
+
+    @Override
+    public long toInteger(int idx) {
+        Long i = toIntegerX(idx);
+        return i == null ? 0 : i;
+    }
+
+    @Override
+    public Long toIntegerX(int idx) {
+        Object val = stack.get(idx);
+        return val instanceof Long ? (Long) val : null;
+    }
+
+    @Override
+    public double toNumber(int idx) {
+        Double n = toNumberX(idx);
+        return n == null ? 0 : n;
+    }
+
+    @Override
+    public Double toNumberX(int idx) {
+        Object val = stack.get(idx);
+        if (val instanceof Double) {
+            return (Double) val;
+        } else if (val instanceof Long) {
+            return ((Long) val).doubleValue();
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public String toString(int idx) {
+        Object val = stack.get(idx);
+        if (val instanceof String) {
+            return (String) val;
+        } else if (val instanceof Long || val instanceof Double) {
+            return val.toString();
+        } else {
+            return null;
+        }
+    }
+
+    /* push functions (Go -> stack); */
+
+    @Override
+    public void pushNil() {
+        stack.push(null);
+    }
+
+    @Override
+    public void pushBoolean(boolean b) {
+        stack.push(b);
+    }
+
+    @Override
+    public void pushInteger(long n) {
+        stack.push(n);
+    }
+
+    @Override
+    public void pushNumber(double n) {
+        stack.push(n);
+    }
+
+    @Override
+    public void pushString(String s) {
+        stack.push(s);
+    }
+
+    /* comparison and arithmetic functions */
+
+    @Override
+    public void arith(ArithOp op) {
+        Object b = stack.pop();
+        Object a = op != LUA_OPUNM && op != LUA_OPBNOT ? stack.pop() : b;
+        Object result = Arithmetic.arith(a, b, op);
+        if (result != null) {
+            stack.push(result);
+        } else {
+            throw new RuntimeException("arithmetic error!");
+        }
+    }
+
+    @Override
+    public boolean compare(int idx1, int idx2, CmpOp op) {
+        if (!stack.isValid(idx1) || !stack.isValid(idx2)) {
+            return false;
+        }
+
+        Object a = stack.get(idx1);
+        Object b = stack.get(idx2);
+        switch (op) {
+            case LUA_OPEQ: return Comparison.eq(a, b);
+            case LUA_OPLT: return Comparison.lt(a, b);
+            case LUA_OPLE: return Comparison.le(a, b);
+            default: throw new RuntimeException("invalid compare op!");
+        }
+    }
+
+    /* miscellaneous functions */
+
+    @Override
+    public void len(int idx) {
+        Object val = stack.get(idx);
+        if (val instanceof String) {
+            pushInteger(((String) val).length());
+        } else {
+            throw new RuntimeException("length error!");
+        }
+    }
+
+    @Override
+    public void concat(int n) {
+        if (n == 0) {
+            stack.push("");
+        } else if (n >= 2) {
+            for (int i = 1; i < n; i++) {
+                if (isString(-1) && isString(-2)) {
+                    String s2 = toString(-1);
+                    String s1 = toString(-2);
+                    pop(2);
+                    pushString(s1 + s2);
+                    continue;
+                }
+
+                throw new RuntimeException("concatenation error!");
+            }
+        }
+        // n == 1, do nothing
+    }
+
+    /* LuaVM */
+
+    @Override
+    public int getPC() {
+        return pc;
+    }
+
+    @Override
+    public void addPC(int n) {
+        pc += n;
+    }
+
+    @Override
+    public int fetch() {
+        return proto.getCode()[pc++];
+    }
+
+    @Override
+    public void getConst(int idx) {
+        stack.push(proto.getConstants()[idx]);
+    }
+
+    @Override
+    public void getRK(int rk) {
+        if (rk > 0xFF) { // constant
+            getConst(rk & 0xFF);
+        } else { // register
+            pushValue(rk + 1);
+        }
+    }
+
+}
