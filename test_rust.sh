2316c04e5452c06ee8cd3e1ed7a552ab5a78c741
zxh0 <zhangxiuhong2013@gmail.com>
2019-01-14 21:42:03 +0800

    + test_rust.sh

---
+++ test_rust.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -ex
+
+BOOK=$PWD
+RUST=$PWD/code/rust
+LUA=$PWD/code/lua
+
+luac -o hw.luac   $LUA/ch02/hello_world.lua
+# luac -o sum.luac  $LUA/ch06/sum.lua
+# luac -o ch07.luac $LUA/ch07/test.lua
+# luac -o ch08.luac $LUA/ch08/test.lua
+# luac -o fact.luac $LUA/ch10/factorial.lua
+# luac -o fib.luac  $LUA/ch10/fibonacci.lua
+# luac -o ch10.luac $LUA/ch10/test.lua
+# luac -o vec2.luac $LUA/ch11/vector2.lua
+# luac -o ch12.luac $LUA/ch12/test.lua
+# luac -o ch13.luac $LUA/ch13/test.lua
+
+cd $RUST/ch01
+cd ../ch01; cargo run 2>&1 | grep -q 'Hello, world!'
+
+rm $BOOK/*.luac
+echo OK
