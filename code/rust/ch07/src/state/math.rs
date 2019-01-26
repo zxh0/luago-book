f3cccf9da96142f853e668a09ea0e3090559dc40
zxh0 <zhangxiuhong2013@gmail.com>
2019-01-26 16:18:06 +0800

    update rust/ch07

--- code/rust/ch07/src/state/math.rs
+++ code/rust/ch07/src/state/math.rs
@@ -1,3 +1,9 @@
+// https://users.rust-lang.org/t/random-number-without-using-the-external-crate/17260/8
+pub fn random() -> usize {
+    let ptr = Box::into_raw(Box::new(123));
+    ptr as usize
+}
+
 pub fn float_to_integer(n: f64) -> Option<i64> {
     let i = n as i64;
     if i as f64 == n {
