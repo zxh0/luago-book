9e55e8a7953d9a4e67267d8e638a27e4407b9231
zxh0 <zhangxiuhong2013@gmail.com>
2019-01-29 20:56:41 +0800

    + rust/ch09

---
+++ code/rust/ch09/src/state/lua_table.rs
@@ -0,0 +1,117 @@
+use super::lua_value::LuaValue;
+use std::collections::HashMap;
+use std::hash::{Hash, Hasher};
+
+#[derive(Clone)]
+pub struct LuaTable {
+    arr: Vec<LuaValue>,
+    map: HashMap<LuaValue, LuaValue>,
+    rdm: usize, // hash code
+}
+
+impl Hash for LuaTable {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.rdm.hash(state);
+    }
+}
+
+impl LuaTable {
+    pub fn new(narr: usize, nrec: usize) -> LuaTable {
+        LuaTable {
+            arr: Vec::with_capacity(narr),
+            map: HashMap::with_capacity(nrec),
+            rdm: super::math::random(),
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        self.arr.len()
+    }
+
+    pub fn get(&self, key: &LuaValue) -> LuaValue {
+        if let Some(idx) = to_index(key) {
+            if idx <= self.arr.len() {
+                return self.arr[idx - 1].clone(); // TODO
+            }
+        }
+        if let Some(val) = self.map.get(key) {
+            val.clone() // TODO
+        } else {
+            LuaValue::Nil
+        }
+    }
+
+    pub fn put(&mut self, key: LuaValue, val: LuaValue) {
+        if key.is_nil() {
+            panic!("table index is nil!");
+        }
+        if let LuaValue::Number(n) = key {
+            if n.is_nan() {
+                panic!("table index is NaN!");
+            }
+        }
+
+        if let Some(idx) = to_index(&key) {
+            let arr_len = self.arr.len();
+            if idx <= arr_len {
+                let val_is_nil = val.is_nil();
+                self.arr[idx - 1] = val;
+                if idx == arr_len && val_is_nil {
+                    self.shrink_array();
+                }
+                return;
+            }
+            if idx == arr_len + 1 {
+                self.map.remove(&key);
+                if !val.is_nil() {
+                    self.arr.push(val);
+                    self.expand_array();
+                }
+                return;
+            }
+        }
+
+        if !val.is_nil() {
+            self.map.insert(key, val);
+        } else {
+            self.map.remove(&key);
+        }
+    }
+
+    fn shrink_array(&mut self) {
+        while !self.arr.is_empty() {
+            if self.arr.last().unwrap().is_nil() {
+                self.arr.pop();
+            }
+        }
+    }
+
+    fn expand_array(&mut self) {
+        let mut idx = self.arr.len() + 1;
+        loop {
+            let key = LuaValue::Integer(idx as i64);
+            if self.map.contains_key(&key) {
+                let val = self.map.remove(&key).unwrap();
+                self.arr.push(val);
+                idx += 1;
+            } else {
+                break;
+            }
+        }
+    }
+}
+
+fn to_index(key: &LuaValue) -> Option<usize> {
+    if let LuaValue::Integer(i) = key {
+        if *i >= 1 {
+            return Some(*i as usize);
+        }
+    } else if let LuaValue::Number(n) = key {
+        if let Some(i) = super::math::float_to_integer(*n) {
+            if i >= 1 {
+                return Some(i as usize);
+            }
+        }
+    }
+    None
+}
---
+++ code/rust/ch09/src/vm/instr_table.rs
@@ -0,0 +1,76 @@
+use super::fpb::fb2int;
+use super::instruction::Instruction;
+use crate::api::LuaVM;
+
+/* number of list items to accumulate before a SETLIST instruction */
+const LFIELDS_PER_FLUSH: isize = 50;
+
+// R(A) := {} (size = B,C)
+pub fn new_table(i: u32, vm: &mut LuaVM) {
+    let (mut a, b, c) = i.abc();
+    a += 1;
+
+    let narr = fb2int(b as usize);
+    let nrec = fb2int(c as usize);
+    vm.create_table(narr, nrec);
+    vm.replace(a);
+}
+
+// R(A) := R(B)[RK(C)]
+pub fn get_table(i: u32, vm: &mut LuaVM) {
+    let (mut a, mut b, c) = i.abc();
+    a += 1;
+    b += 1;
+
+    vm.get_rk(c);
+    vm.get_table(b);
+    vm.replace(a);
+}
+
+// R(A)[RK(B)] := RK(C)
+pub fn set_table(i: u32, vm: &mut LuaVM) {
+    let (mut a, b, c) = i.abc();
+    a += 1;
+
+    vm.get_rk(b);
+    vm.get_rk(c);
+    vm.set_table(a);
+}
+
+// R(A)[(C-1)*FPF+i] := R(A+i), 1 <= i <= B
+pub fn set_list(i: u32, vm: &mut LuaVM) {
+    let (mut a, mut b, mut c) = i.abc();
+    a += 1;
+
+    if c > 0 {
+        c = c - 1;
+    } else {
+        c = vm.fetch().ax();
+    }
+
+    let b_is_zero = b == 0;
+    if b_is_zero {
+        b = vm.to_integer(-1) as isize - a - 1;
+        vm.pop(1);
+    }
+
+    vm.check_stack(1);
+    let mut idx = (c * LFIELDS_PER_FLUSH) as i64;
+    for j in 1..(b + 1) {
+        idx += 1;
+        vm.push_value(a + j);
+        vm.set_i(a, idx);
+    }
+
+    if b_is_zero {
+        let nreg = vm.register_count() as isize;
+        for j in (nreg + 1)..(vm.get_top() + 1) {
+            idx += 1;
+            vm.push_value(j);
+            vm.set_i(a, idx);
+        }
+
+        // clear stack
+        vm.set_top(nreg);
+    }
+}
