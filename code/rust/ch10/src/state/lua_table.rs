6a5220a55061bb0a49eefc11a96bf5499de15547
zxh0 <zhangxiuhong2013@gmail.com>
2019-02-02 16:09:44 +0800

    simplify LuaValue

--- code/rust/ch10/src/state/lua_table.rs
+++ code/rust/ch10/src/state/lua_table.rs
@@ -1,11 +1,13 @@
 use super::lua_value::LuaValue;
+use std::cell::RefCell;
 use std::collections::HashMap;
 use std::hash::{Hash, Hasher};
 
+// https://doc.rust-lang.org/std/cell/index.html#introducing-mutability-inside-of-something-immutable
 #[derive(Clone)]
 pub struct LuaTable {
-    arr: Vec<LuaValue>,
-    map: HashMap<LuaValue, LuaValue>,
+    arr: RefCell<Vec<LuaValue>>,
+    map: RefCell<HashMap<LuaValue, LuaValue>>,
     rdm: usize, // hash code
 }
 
@@ -18,30 +20,33 @@ impl Hash for LuaTable {
 impl LuaTable {
     pub fn new(narr: usize, nrec: usize) -> LuaTable {
         LuaTable {
-            arr: Vec::with_capacity(narr),
-            map: HashMap::with_capacity(nrec),
+            arr: RefCell::new(Vec::with_capacity(narr)),
+            map: RefCell::new(HashMap::with_capacity(nrec)),
             rdm: super::math::random(),
         }
     }
 
     pub fn len(&self) -> usize {
-        self.arr.len()
+        self.arr.borrow().len()
     }
 
     pub fn get(&self, key: &LuaValue) -> LuaValue {
+        let arr = self.arr.borrow();
         if let Some(idx) = to_index(key) {
-            if idx <= self.arr.len() {
-                return self.arr[idx - 1].clone(); // TODO
+            if idx <= arr.len() {
+                return arr[idx - 1].clone(); // TODO
             }
         }
-        if let Some(val) = self.map.get(key) {
+
+        let map = self.map.borrow();
+        if let Some(val) = map.get(key) {
             val.clone() // TODO
         } else {
             LuaValue::Nil
         }
     }
 
-    pub fn put(&mut self, key: LuaValue, val: LuaValue) {
+    pub fn put(&self, key: LuaValue, val: LuaValue) {
         if key.is_nil() {
             panic!("table index is nil!");
         }
@@ -51,52 +56,32 @@ impl LuaTable {
             }
         }
 
+        let mut arr = self.arr.borrow_mut();
+        let mut map = self.map.borrow_mut();
         if let Some(idx) = to_index(&key) {
-            let arr_len = self.arr.len();
+            let arr_len = arr.len();
             if idx <= arr_len {
                 let val_is_nil = val.is_nil();
-                self.arr[idx - 1] = val;
+                arr[idx - 1] = val;
                 if idx == arr_len && val_is_nil {
-                    self.shrink_array();
+                    shrink_array(&mut arr);
                 }
                 return;
             }
             if idx == arr_len + 1 {
-                self.map.remove(&key);
+                map.remove(&key);
                 if !val.is_nil() {
-                    self.arr.push(val);
-                    self.expand_array();
+                    arr.push(val);
+                    expand_array(&mut arr, &mut map);
                 }
                 return;
             }
         }
 
         if !val.is_nil() {
-            self.map.insert(key, val);
+            map.insert(key, val);
         } else {
-            self.map.remove(&key);
-        }
-    }
-
-    fn shrink_array(&mut self) {
-        while !self.arr.is_empty() {
-            if self.arr.last().unwrap().is_nil() {
-                self.arr.pop();
-            }
-        }
-    }
-
-    fn expand_array(&mut self) {
-        let mut idx = self.arr.len() + 1;
-        loop {
-            let key = LuaValue::Integer(idx as i64);
-            if self.map.contains_key(&key) {
-                let val = self.map.remove(&key).unwrap();
-                self.arr.push(val);
-                idx += 1;
-            } else {
-                break;
-            }
+            map.remove(&key);
         }
     }
 }
@@ -114,4 +99,28 @@ fn to_index(key: &LuaValue) -> Option<usize> {
         }
     }
     None
+}
+
+fn shrink_array(arr: &mut Vec<LuaValue>) {
+    while !arr.is_empty() {
+        if arr.last().unwrap().is_nil() {
+            arr.pop();
+        } else {
+            break;
+        }
+    }
+}
+
+fn expand_array(arr: &mut Vec<LuaValue>, map: &mut HashMap<LuaValue, LuaValue>) {
+    let mut idx = arr.len() + 1;
+    loop {
+        let key = LuaValue::Integer(idx as i64);
+        if map.contains_key(&key) {
+            let val = map.remove(&key).unwrap();
+            arr.push(val);
+            idx += 1;
+        } else {
+            break;
+        }
+    }
 }
